c88894b247cdcdb19aab0f0f75615e08efe3a11c
J <1001l1000l1110@gmail.com>
2023-04-19 11:16:16 +0900

    슬라이딩 윈도우 기반 차선 인식 주행

--- 5-4./슬라이딩 윈도우 기반 차선 인식 주행/sliding_test.py
+++ 5-4./슬라이딩 윈도우 기반 차선 인식 주행/sliding_test.py
@@ -1,6 +1,3 @@
-# VideoCapture() 인자값을 동영상 경로로 설정해주시면 실행 가능합니다!
-# 변경한 소스 코드는 녹색 배경으로 표시해두었습니다.
-
 import cv2, time
 import numpy as np
 import random
@@ -10,11 +7,13 @@ image_width, image_height = 640, 480
 warp_image_width = 320
 warp_image_height = 240
 
-num_sliding_window = 10
-area_sliding_window = 12
-min_points = 30 # max: 24 * 24
+# sliding window height = warp_image_height / num_sliding_window
+# sliding window width = width_sliding_window * 2
+num_sliding_window = 20
+width_sliding_window = 20
+min_points = 5
 
-warp_x_margin = 0
+warp_x_margin = 20
 warp_y_margin = 3
 
 lane_bin_th = 145
@@ -39,24 +38,34 @@ warp_src = np.array([
 ], dtype = np.float32)
 
 warp_dist = np.array([
-    [0, 0],
-    [0, warp_image_height],
-    [warp_image_width, 0],
-    [warp_image_width, warp_image_height]
+[0               , 0],
+[0               , warp_image_height],
+[warp_image_width, 0],
+[warp_image_width, warp_image_height],
 ], dtype = np.float32)
 
-capture = cv2.VideoCapture("C:/Users/HP/Desktop/ws/track2.avi")
+capture = cv2.VideoCapture("/Users/1001l1000/Documents/Dev-Course/5-4./슬라이딩 윈도우 기반 차선 인식 주행/xycar_track2.mp4")
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 def onChange(pos):
     pass
 
+'''
+--- TrackBar ---
+
+cv2.namedWindow("Trackbar Windows")
+cv2.createTrackbar("threshold", "Trackbar Windows", 0, 255, onChange)
+cv2.createTrackbar("minimum_points", "Trackbar Windows", 0, 24 * 24, onChange)
+cv2.setTrackbarPos("threshold", "Trackbar Windows", lane_bin_th)
+cv2.setTrackbarPos("minimum_points", "Trackbar Windows", min_points)
+'''
+
 def calibrate_image(src):
     global image_width, image_height
 
     calibrate_mtx = np.array([
-        [350.354184, 0.0, 328.104147],
+        [ 350.354184, 0.0, 328.104147],
         [0.0, 350.652653, 236.540676],
         [0.0, 0.0, 1.0]
     ])
@@ -75,7 +84,6 @@ def warp_image(img, src, dst, size):
     src_to_dst_mtx = cv2.getPerspectiveTransform(src, dst)
     dst_to_src_mtx = cv2.getPerspectiveTransform(dst, src)
     warp_img = cv2.warpPerspective(img, src_to_dst_mtx, size, flags = cv2.INTER_LINEAR)
-    
     return warp_img, src_to_dst_mtx, dst_to_src_mtx
 
 def draw_lines(img,x,y):
@@ -90,7 +98,7 @@ def draw_lines(img,x,y):
 
 def warp_process_image(image):
     global num_sliding_window
-    global area_sliding_window
+    global width_sliding_window
     global min_points
     global lane_bin_th
 
@@ -105,20 +113,49 @@ def warp_process_image(image):
 
     out_img = np.dstack((lane, lane, lane)) * 255
 
-    dividen_lane = np.split(lane, 10, axis = 0) # lane 이미지를 각 sliding window마다 적용 가능하도록 10개로 자름
+    dividen_lane = np.split(lane, num_sliding_window, axis = 0) # lane 이미지를 각 sliding window마다 적용 가능하도록 10개로 자름
+
+    window_margin = 20  # 아래 윈도우의 x좌표보다 +- 50픽셀 이내의 다음 점을 찾기 위한 마진값
+
+    lane_width = 200    # 차선 평균 픽셀 너비 = 240
 
     lx, ly, rx, ry = [], [], [], []
     l_box_center, r_box_center = [], []
+    before_l_detected, before_r_detected = True, True
 
-    for window in range(num_sliding_window):
+    for window in range(num_sliding_window - 1, 0, -1):
         left_lane_inds = []
         right_lane_inds = []
         histogram = np.sum(dividen_lane[window], axis = 0)
         midpoint = np.int32(histogram.shape[0] / 2)
+        r_min_points, l_min_points = min_points, min_points
 
-        leftx_current = np.argmax(histogram[:midpoint])
-        rightx_current = np.argmax(histogram[midpoint:]) + midpoint
+        if window == num_sliding_window - 1:        # 첫 window를 기반으로 그리기 위해 첫 window는 화면 중앙을 기준으로 좌, 우 차선을 나눔
+            leftx_current = np.argmax(histogram[:midpoint])
+            rightx_current = np.argmax(histogram[midpoint:]) + midpoint
+            
+        elif before_l_detected == True and before_r_detected == True:   # 이전 window에서 차선을 둘 다 인식했을 때 이번 window에서는 이전 윈도우의 x값 +- margin값 이내의 윈도우를 찾아냅니다.
+            leftx_current = np.argmax(histogram[: lx[-1] + window_margin])
+            rightx_current = np.argmax(histogram[rx[-1] - window_margin : rx[-1] + window_margin]) + rx[-1] - window_margin
 
+        elif before_l_detected == False and before_r_detected == True:
+            if rx[-1]  - lane_width > 0:
+                leftx_current = np.argmax(histogram[:rx[-1] - lane_width])
+                rightx_current = np.argmax(histogram[rx[-1] - window_margin :]) + histogram[rx[-1] - window_margin]
+                if abs(leftx_current - rightx_current) < 100:
+                    l_min_points = (width_sliding_window * 2) * (warp_image_height / num_sliding_window)
+
+        elif before_l_detected == True and before_r_detected == False:   # 이전 window에서 차선을 하나만 인식했을 때, 있던 차선은 margin값 이내에서, 다른 차선은 lane width만큼 이동 후 찾아냅니다.
+            leftx_current = np.argmax(histogram[max(0, lx[-1] - window_margin):lx[-1] + window_margin]) + max(0, lx[-1] - window_margin)
+            rightx_current = np.argmax(histogram[min(lx[-1] + lane_width, histogram.shape[0] - 1):]) + min(lx[-1] + lane_width, histogram.shape[0] - 1)
+
+            if rightx_current - leftx_current < 100:
+                    r_min_points = (width_sliding_window * 2) * (warp_image_height / num_sliding_window)
+
+        elif before_l_detected == False and before_r_detected == False:
+            leftx_current = np.argmax(histogram[:midpoint])
+            rightx_current = np.argmax(histogram[midpoint:]) + midpoint
+        
         window_height = np.int32(lane.shape[0] / num_sliding_window)    # window_height = 24
 
         nz = dividen_lane[window].nonzero()
@@ -126,10 +163,10 @@ def warp_process_image(image):
         win_yl = (window + 1) * window_height
         win_yh = window * window_height
 
-        win_xll = leftx_current - area_sliding_window
-        win_xlh = leftx_current + area_sliding_window
-        win_xrl = rightx_current - area_sliding_window
-        win_xrh = rightx_current + area_sliding_window
+        win_xll = leftx_current - width_sliding_window
+        win_xlh = leftx_current + width_sliding_window
+        win_xrl = rightx_current - width_sliding_window
+        win_xrh = rightx_current + width_sliding_window
 
         # cv2.rectangle(out_img, (win_xll, win_yl), (win_xlh, win_yh), (0, 255, 0), 2)
         # cv2.rectangle(out_img, (win_xrl, win_yl), (win_xrh, win_yh), (0, 255, 0), 2)
@@ -140,15 +177,21 @@ def warp_process_image(image):
         left_lane_inds.append(good_left_inds)
         right_lane_inds.append(good_right_inds)
 
-        if len(good_left_inds) > min_points:
+        if len(good_left_inds) > l_min_points:
             leftx_current = np.int32(np.mean(nz[1][good_left_inds]))
             cv2.rectangle(out_img, (win_xll, win_yl), (win_xlh, win_yh), (0, 255, 0), 2)
             l_box_center.append([(win_xll + win_xlh) // 2, (win_yl + win_yh) // 2])
-        
-        if len(good_right_inds) > min_points:
+            before_l_detected = True
+        else:
+            before_l_detected = False
+
+        if len(good_right_inds) > r_min_points:
             rightx_current = np.int32(np.mean(nz[1][good_right_inds]))
             cv2.rectangle(out_img, (win_xrl, win_yl), (win_xrh, win_yh), (0, 255, 0), 2)
             r_box_center.append([(win_xrl + win_xrh) // 2, (win_yl + win_yh) // 2])
+            before_r_detected = True
+        else:
+            before_r_detected = False
 
         lx.append(leftx_current)
         ly.append((win_yl + win_yh) / 2)
@@ -185,13 +228,19 @@ def get_line_pos(lst):
         dx = 0.01
 
     slope = float(y2-y1) / float(dx)
-    
     if abs(slope) > low_slope_threshold and abs(slope) < high_slope_threshold:
         return slope
     else:
         return float("inf")
 
 while True:
+
+    ''' 
+    Trackbar
+
+    lane_bin_th = cv2.getTrackbarPos("threshold", "Trackbar Windows")
+    '''
+
     _, src = capture.read()
     cal = calibrate_image(src)
     warp, src_mtx, dst_mtx = warp_image(cal, warp_src, warp_dist, (warp_image_width, warp_image_height))
@@ -209,3 +258,15 @@ while True:
 
     if cv2.waitKey(10) == 27:
         break
+
+
+'''
+--- 수정사항 ---
+
+--- 4.15---
+* sliding window의 개수를 10개로 변경하였습니다.
+
+--- 4.16 ---
+* window를 그리는 순서를 위 -> 아래에서 아래 -> 위로 그리도록 변경하였습니다.
+
+'''
