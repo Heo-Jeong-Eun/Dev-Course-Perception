0af42460b0157deb16a75c950feaa76c51ff3890
J <1001l1000l1110@gmail.com>
2023-05-01 20:23:54 +0900

    특징점 검출과 매칭

---
+++ 8-1./배포 파일/descriptors/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include "opencv2/opencv.hpp"
+#include "opencv2/core/ocl.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	ocl::setUseOpenCL(false); // for ORB time check
+
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "Image load failed!" << endl;
+		return -1;
+	}
+
+	TickMeter tm;
+	tm.start();
+
+	Ptr<Feature2D> feature = SIFT::create(); // SIFT, KAZE, AKAZE, ORB
+
+	vector<KeyPoint> keypoints;
+	Mat desc;
+	feature->detectAndCompute(src, Mat(), keypoints, desc);
+
+	tm.stop();
+	cout << "Elapsed time: " << tm.getTimeMilli() << "ms." << endl;
+	cout << "keypoints.size(): " << keypoints.size() << endl;
+	cout << "desc.size(): " << desc.size() << endl;
+
+	Mat dst;
+	drawKeypoints(src, keypoints, dst, Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+	imshow("dst", dst);
+	waitKey();
+}
---
+++ 8-1./배포 파일/keypoints/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include "opencv2/opencv.hpp"
+#include "opencv2/core/ocl.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	ocl::setUseOpenCL(false); // for ORB time check
+
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "Image load failed!" << endl;
+		return -1;
+	}
+
+	TickMeter tm;
+	tm.start();
+
+	Ptr<Feature2D> detector = SIFT::create(); // SIFT, KAZE, AKAZE, ORB
+
+	vector<KeyPoint> keypoints;
+	detector->detect(src, keypoints);
+
+	tm.stop();
+	cout << "Elapsed time: " << tm.getTimeMilli() << "ms." << endl;
+	cout << "keypoints.size(): " << keypoints.size() << endl;
+
+	Mat dst;
+	drawKeypoints(src, keypoints, dst, Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+	imshow("dst", dst);
+	waitKey();
+}
